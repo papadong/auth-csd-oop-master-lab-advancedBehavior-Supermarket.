d55a991e917ed5db089b42cc14f9cb6819a32b6b
Nikolaos Papadopoulos <papadong@csd.auth.gr>
2021-11-29 18:44:17 +0200

    full

--- src/Supermarket.java
+++ src/Supermarket.java
@@ -4,47 +4,63 @@ import java.util.HashSet;
 /**
  * Η κλάση αυτή αποθηκεύει στη μνήμη τις συναλλαγές σε ένα σούπερ μάρκετ.
  * This class saves in memory all the transactions of a supermarket.
- *
  */
 public class Supermarket {
 
+    ArrayList<Transaction> transactions;
 
-    public Supermarket(){
-        // fill your code here
+    public Supermarket() {
+        transactions = new ArrayList<>();
     }
-   
+
     /**
      * Αυτή η μέθοδος αποθηκεύει μια συναλλαγή
      * This method saves a transaction
-     * 
+     *
      * @param t μια συναλλαγή , a transaction
      */
     public void addTransaction(Transaction t) {
-        // fill your code here
+        transactions.add(t);
     }
-    
+
     /**
-     * Αυτή η μέθοδος επιστρέφει το πλήθος όλων των πωλήσεων ενός προϊόντος σε 
+     * Αυτή η μέθοδος επιστρέφει το πλήθος όλων των πωλήσεων ενός προϊόντος σε
      * όλες τις αποθηκευμένες συναλλαγές.
      * This method returns the total number of sales of a product from all saved transactions.
-     * 
+     *
      * @param product ένα προϊόν , a product
      * @return το πλήθος των πωλήσεων του προϊόντος, the total sales number
      */
     public int getTotalSalesOfProduct(String product) {
-        // fill your code here
+        int counter = 0;
+        for (Transaction transaction : transactions) {
+            for (String productIn : transaction.products) {
+                if (productIn.equals(product))
+                    counter++;
+            }
+        }
+        return counter;
     }
-    
+
     /**
      * Η μέθοδος αυτή επιστρέφει το πλήθος των μοναδικών προϊόντων που έχουν
      * βρεθεί σε συναλλαγές.
      * This method returns the number of unique products that have been found
      * in the transactions.
-     * 
+     *
      * @return Το πλήθος των μοναδικών προϊόντων, the total number of unique products
      */
     public int uniqueProducts() {
-        // fill your code here
+        ArrayList<String> uniqueProducts = new ArrayList<>();
+        for (Transaction transaction : transactions) {
+            int length = transaction.getProducts().length;
+            for (int k = 0; k < length; k++) {
+                String product = transaction.getProducts()[k];
+                if (!uniqueProducts.contains(product))
+                    uniqueProducts.add(product);
+            }
+        }
+        return uniqueProducts.size();
     }
-    
+
 }
--- src/Transaction.java
+++ src/Transaction.java
@@ -1,78 +1,91 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
+import java.awt.image.AreaAveragingScaleFilter;
+import java.util.*;
 
 /**
- * Η κλάση αυτή αφορά μια συναλλαγή ενός πελάτη με ένα supermarket. Με άλλα 
- * λόγια αντιπροσωπεύει το καλάθι με τα προϊόντα που αγόρασε σε μια επίσκεψη. 
+ * Η κλάση αυτή αφορά μια συναλλαγή ενός πελάτη με ένα supermarket. Με άλλα
+ * λόγια αντιπροσωπεύει το καλάθι με τα προϊόντα που αγόρασε σε μια επίσκεψη.
  * This class represents a transaction of a super market customer. In other words,
  * the basket with the products of a visit to the supermarket.
- *
  */
 public class Transaction {
 
- 
+    ArrayList<String> products;
+
     public Transaction() {
-        // συμπληρώστε τον κώδικα
-        // fill your code here
+        products = new ArrayList<>();
     }
-    
+
     /**
-     * Η μέθοδος αυτή αντιστοιχεί στο σκανάρισμα ενός προϊόντος και άρα στην 
+     * Η μέθοδος αυτή αντιστοιχεί στο σκανάρισμα ενός προϊόντος και άρα στην
      * προσθήκη του στην τρέχουσα συναλλαγή ενός πελάτη.
      * This method represents the scanning process in a supermarket. It adds the product
      * to the current transaction.
-     * 
+     *
      * @param product συμβολοσειρά με το όνομα του προϊόντος, π.χ. γάλα, string containing the name of
      *                the product e.g. milk
      */
     public void scanItem(String product) {
-        // συμπληρώστε τον κώδικα
-        // fill your code here
+        products.add(product);
     }
 
     /**
      * Η μέθοδος αυτή αντιστοιχεί στο σκανάρισμα πολλών προϊόντων του ίδιου
-     * είδους και προσθήκη τους στην τρέχουσα συναλλαγή ενός πελάτη. 
-     *
+     * είδους και προσθήκη τους στην τρέχουσα συναλλαγή ενός πελάτη.
+     * <p>
      * This method represents the scanning of the same product multiple times
      * and adds them to the customers transactions.
      *
      * @param product συμβολοσειρά με το όνομα του προϊόντος, π.χ. γάλα, string containing the name of
-     *               the product e.g. milk
-     * @param amount ποσότητα προϊόντος, the amount of the products
+     *                the product e.g. milk
+     * @param amount  ποσότητα προϊόντος, the amount of the products
      */
     public void scanItems(String product, int amount) {
-        // συμπληρώστε τον κώδικα
-        // fill your code here
+        for (int k = 0; k < amount; k++) {
+            products.add(product);
+        }
     }
-    
+
     /**
-     * Η μέθοδος αυτή επιστρέφει τo πλήθος εμφάνισης ενός προϊόντος στο 
-     * καλάθι ενός πελάτη. 
+     * Η μέθοδος αυτή επιστρέφει τo πλήθος εμφάνισης ενός προϊόντος στο
+     * καλάθι ενός πελάτη.
      * The number of times a product appears in the basket.
      *
      * @param product συμβολοσειρά με το όνομα του προϊόντος, π.χ. γάλα, string containing the name of
-     *               the product e.g. milk
+     *                the product e.g. milk
      */
     public int getAmountOfProduct(String product) {
-        // συμπληρώστε τον κώδικα
-        // fill your code here
+        int size = products.size();
+        int counter = 0;
+        for (String s : products) {
+            if (s.equals(product))
+                counter++;
+        }
+        return counter;
     }
-    
+
     /**
      * Η μέθοδος αυτή επιστέφει έναν πίνακα με τα ονόματα των προϊόντων που
-     * υπάρχουν στο καλάθι του πελάτη. Αν το ίδιο προϊόν υπάρχει πάνω από μία 
-     * φορές στο καλάθι, θα πρέπει στον επιστρεφόμενο πίνακα να εμφανίζεται μία 
+     * υπάρχουν στο καλάθι του πελάτη. Αν το ίδιο προϊόν υπάρχει πάνω από μία
+     * φορές στο καλάθι, θα πρέπει στον επιστρεφόμενο πίνακα να εμφανίζεται μία
      * φορά μόνο.
-     *
+     * <p>
      * This method returns a table with the names of the products that exist in the basket.
      * The returning table should not contain duplicate items and each product should appear only once.
-     * 
+     *
      * @return ο πίνακας με τα ονόματα των προϊόντων, the table with the names of the products purchased.
      */
     public String[] getProducts() {
-        // συμπληρώστε τον κώδικα
-        // fill your code here
-    }    
+        ArrayList<String> uniqueProducts = new ArrayList<>();
+        for (String product : products) {
+            if (!uniqueProducts.contains(product))
+                uniqueProducts.add(product);
+        }
+
+        int listSize = uniqueProducts.size();
+        String[] list = new String[listSize];
+        for (int k = 0; k < listSize; k++) {
+            list[k] = uniqueProducts.get(k);
+        }
+        return list;
+    }
 }
